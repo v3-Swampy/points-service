e0af3640df0dbba1dee7a7cab6cc96c7460b6ad2
Bo QIU <35757521+boqiu@users.noreply.github.com>
2025-09-25 13:49:28 +0800

    Update token price strategy (#26)
    * Handle 0 price in swappi and vswap
    
    * Update token price sample strategy

--- blockchain/vswap.go
+++ blockchain/vswap.go
@@ -23,6 +23,11 @@ func (vswap *Vswap) GetPoolInfo(pool common.Address) (PairInfo, error) {
 	return vswap.swappi.GetPairInfo(pool)
 }
 
+// GetTokenPrice calculates the price of given token in pool.
+//
+// It will returns error if the given token not found in pool.
+//
+// Note, it returns 0 if pool balance of any token is 0.
 func (vswap *Vswap) GetTokenPrice(opts *bind.CallOpts, pool, token common.Address) (decimal.Decimal, error) {
 	// get pool info
 	info, err := vswap.GetPoolInfo(pool)
@@ -41,6 +46,10 @@ func (vswap *Vswap) GetTokenPrice(opts *bind.CallOpts, pool, token common.Addres
 		return decimal.Zero, errors.WithMessage(err, "Failed to get pool balance of token1")
 	}
 
+	if balance0.IsZero() || balance1.IsZero() {
+		return decimal.Zero, nil
+	}
+
 	// calculate price
 	if token == info.Token0.Address {
 		return balance1.Div(balance0), nil
@@ -53,6 +62,11 @@ func (vswap *Vswap) GetTokenPrice(opts *bind.CallOpts, pool, token common.Addres
 	return decimal.Zero, errors.Errorf("Token not found in pool %v", info)
 }
 
+// GetTokenPrice calculates the USDT price of given token in pool.
+//
+// It will returns error if the given token not found in pool.
+//
+// Note, it returns 0 if pool balance of any token is 0.
 func (vswap *Vswap) GetTokenPriceUSDT(opts *bind.CallOpts, pool, token common.Address) (decimal.Decimal, error) {
 	if token == vswap.swappi.addresses.USDT {
 		return decimal.NewFromInt(1), nil
@@ -93,6 +107,10 @@ func (vswap *Vswap) GetTokenPriceUSDT(opts *bind.CallOpts, pool, token common.Ad
 		return decimal.Zero, errors.WithMessage(err, "Failed to calculate token price by token/WCFX")
 	}
 
+	if wcfxPrice.IsZero() {
+		return decimal.Zero, nil
+	}
+
 	usdtPrice, err := vswap.GetTokenPrice(opts, vswap.wcfxUsdtPool, vswap.swappi.addresses.WCFX)
 	if err != nil {
 		return decimal.Zero, errors.WithMessage(err, "Failed to calcuate WCFX price by WCFX/USDT")
@@ -107,17 +125,6 @@ func (vswap *Vswap) GetPoolTVL(opts *bind.CallOpts, pool common.Address) (decima
 		return decimal.Zero, errors.WithMessage(err, "Failed to get pool info")
 	}
 
-	// get prices
-	price0, err := vswap.GetTokenPriceUSDT(opts, pool, info.Token0.Address)
-	if err != nil {
-		return decimal.Zero, errors.WithMessage(err, "Failed to get price of token0")
-	}
-
-	price1, err := vswap.GetTokenPriceUSDT(opts, pool, info.Token1.Address)
-	if err != nil {
-		return decimal.Zero, errors.WithMessage(err, "Failed to get price of token1")
-	}
-
 	// get balances
 	balance0, err := vswap.swappi.erc20.GetBalance(opts, info.Token0.Address, pool)
 	if err != nil {
@@ -129,9 +136,24 @@ func (vswap *Vswap) GetPoolTVL(opts *bind.CallOpts, pool common.Address) (decima
 		return decimal.Zero, errors.WithMessage(err, "Failed to get pool balance of token1")
 	}
 
-	// calculate TVL
-	value0 := balance0.Mul(price0)
-	value1 := balance1.Mul(price1)
+	if balance0.IsZero() || balance1.IsZero() {
+		return decimal.Zero, nil
+	}
 
-	return value0.Add(value1), nil
+	// get prices
+	price0, err := vswap.GetTokenPriceUSDT(opts, pool, info.Token0.Address)
+	if err != nil {
+		return decimal.Zero, errors.WithMessage(err, "Failed to get price of token0")
+	}
+
+	price1, err := vswap.GetTokenPriceUSDT(opts, pool, info.Token1.Address)
+	if err != nil {
+		return decimal.Zero, errors.WithMessage(err, "Failed to get price of token1")
+	}
+
+	// calculate TVL
+	tvl0 := balance0.Mul(price0)
+	tvl1 := balance1.Mul(price1)
+
+	return tvl0.Add(tvl1), nil
 }
